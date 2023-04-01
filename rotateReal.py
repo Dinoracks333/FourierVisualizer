44e9ad8503b4a54e0d39fbd49309b59daa02ba0d
Alejandro Casillas <40367952+Dinoracks333@users.noreply.github.com>
2023-04-01 17:31:29 -0400

    Update rotateReal.py

--- rotateReal.py
+++ rotateReal.py
@@ -1,3 +1,5 @@
+#python rotation creates a bigger box image and warps it, so it is very incorrect
+#This monstrosity corrects it
 import math
 def rRotate(x,y,width,height,angle,axis): 
   offy=(width*abs(math.sin(angle*math.pi/180)))/2
@@ -5,4 +7,3 @@ def rRotate(x,y,width,height,angle,axis):
   a=[offy,offy+height*abs(math.cos(angle*math.pi/180)),offy+height*abs(math.cos(angle*math.pi/180)),offy][(int(angle//90+axis))%4]
   b=[offx,offx,offx+height*abs(math.sin(angle*math.pi/180)),offx+height*abs(math.sin(angle*math.pi/180))][(int(angle//90+axis))%4]
   return (x-b,y-a)
-#only axis 0 and 2 works rn
